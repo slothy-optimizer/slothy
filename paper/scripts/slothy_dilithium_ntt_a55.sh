e400854e26cff7640e8339340426c1ae11e66ac4
Hanno Becker <hannobecker@posteo.de>
2023-11-28 07:14:37 +0000

    Add missing inputs_are_outputs config in Dilithium NTT script

--- paper/scripts/slothy_dilithium_ntt_a55.sh
+++ paper/scripts/slothy_dilithium_ntt_a55.sh
@@ -30,6 +30,7 @@ time ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                        
          -l layer123_start                                                      \
          -r ntt_dilithium_123_45678,ntt_dilithium_123_45678_opt0_a55            \
          -c sw_pipelining.enabled=true                                          \
+         -c inputs_are_outputs                                                  \
          -c reserved_regs="[x0,x1,x2,x3,x4,x5,x6,v8,x30,sp]"                    \
          -c sw_pipelining.minimize_overlapping=False                            \
          -c constraints.stalls_first_attempt=110 -c variable_size               \
@@ -43,6 +44,7 @@ time ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                       \
          -o ${OPT_DIR}/neon/ntt_dilithium_123_45678_opt_a55.s                  \
          -r ntt_dilithium_123_45678_opt0_a55,ntt_dilithium_123_45678_opt_a55   \
          -c sw_pipelining.enabled=true                                         \
+         -c inputs_are_outputs                                                 \
          -c reserved_regs="[x3,x30,sp]"                                        \
          -c sw_pipelining.minimize_overlapping=False                           \
          -c constraints.stalls_first_attempt=40                                \
@@ -57,6 +59,7 @@ time ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                        
          -o ${OPT_DIR}/neon/ntt_dilithium_123_45678_w_scalar_opt0_a55.s                 \
          -r ntt_dilithium_123_45678_w_scalar,ntt_dilithium_123_45678_w_scalar_opt0_a55  \
          -c sw_pipelining.enabled=true                                                  \
+         -c inputs_are_outputs                                                          \
          -c reserved_regs="[x0,x1,x2,x3,x4,x5,x6,v8,x30,sp]"                            \
          -c sw_pipelining.minimize_overlapping=False                                    \
          -c constraints.stalls_first_attempt=110 -c variable_size                       \
@@ -68,6 +71,7 @@ time ${SLOTHY_DIR}/slothy-cli Arm_AArch64 Arm_Cortex_A55                        
             ${OPT_DIR}/neon/ntt_dilithium_123_45678_w_scalar_opt0_a55.s                        \
          -l layer45678_start                                                                   \
          -c sw_pipelining.enabled=true                                                         \
+         -c inputs_are_outputs                                                                 \
          -o ${OPT_DIR}/neon/ntt_dilithium_123_45678_w_scalar_opt_a55.s                         \
          -r ntt_dilithium_123_45678_w_scalar_opt0_a55,ntt_dilithium_123_45678_w_scalar_opt_a55 \
          -c reserved_regs="[x3,x30,sp]"                                                        \
