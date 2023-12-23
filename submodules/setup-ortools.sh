10d22bec81e70cd0076e53c0f12ba7aa61d514a6
Hanno Becker <beckphan@amazon.co.uk>
2023-12-23 05:38:46 +0000

    Add or-tools patch working around build issue

--- submodules/setup-ortools.sh
+++ submodules/setup-ortools.sh
@@ -4,11 +4,14 @@ git submodule init
 git submodule update
 
 cd or-tools
+
+# Work around https://github.com/google/or-tools/issues/4027
+git apply ../0001-Pin-pybind11_protobuf-commit-in-cmake-files.patch
+
 mkdir build
 cmake -S. -Bbuild -DBUILD_PYTHON:BOOL=ON
 cd build
 make -j8
-
 source python/venv/bin/activate
 pip3 install sympy
 deactivate
