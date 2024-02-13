689b132b0c9f75e03277c536849dc279140050bc
Amin Abdulrahman <amin@abdulrahman.de>
2024-02-13 17:44:46 +0100

    Experimental Apple M1 Model and Example

---
+++ slothy/targets/aarch64/apple_m1_firestorm.py
@@ -0,0 +1,206 @@
+#
+# Copyright (c) 2022 Arm Limited
+# Copyright (c) 2022 Hanno Becker
+# Copyright (c) 2023 Amin Abdulrahman, Matthias Kannwischer
+# SPDX-License-Identifier: MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Author: Hanno Becker <hannobecker@posteo.de>
+#
+
+#
+# Experimental model for high-end Apple M1 CPU
+# Based on data by https://dougallj.github.io/applecpu/firestorm.html
+#
+
+from enum import Enum
+from itertools import combinations, product
+from slothy.targets.aarch64.aarch64_neon import *
+
+issue_rate = 8
+
+class ExecutionUnit(Enum):
+    SCALAR_I0=0,
+    SCALAR_I1=1,
+    SCALAR_I2=2,
+    SCALAR_I3=3,
+    SCALAR_I4=4, 
+    SCALAR_I5=5, 
+    SCALAR_M0=4,  # Overlaps with fifth I pipeline
+    SCALAR_M1=5,  # Overlaps with sixth I pipeline
+    SU0=6,
+    LSU0=7,
+    LU0=8,
+    LU1=9,
+    VEC0=10,
+    VEC1=11,
+    VEC2=12,
+    VEC3=13,
+    def __repr__(self):
+        return self.name
+    def I():
+        return [ExecutionUnit.SCALAR_I0, ExecutionUnit.SCALAR_I1,
+                ExecutionUnit.SCALAR_I2, ExecutionUnit.SCALAR_I3,
+                ExecutionUnit.SCALAR_I4, ExecutionUnit.SCALAR_I5]
+    def M():
+        return [ExecutionUnit.SCALAR_M0, ExecutionUnit.SCALAR_M1]
+    def V():
+        return [ExecutionUnit.VEC0, ExecutionUnit.VEC1,
+                ExecutionUnit.VEC2, ExecutionUnit.VEC3]
+    def V0():
+        return [ExecutionUnit.VEC0]
+    def V1():
+        return [ExecutionUnit.VEC1]
+    def V13():
+        return [ExecutionUnit.VEC1, ExecutionUnit.VEC3]
+    def V01():
+        return [ExecutionUnit.VEC0, ExecutionUnit.VEC1]
+    def V02():
+        return [ExecutionUnit.VEC0, ExecutionUnit.VEC2]
+    def LSU():
+        return [ExecutionUnit.LSU0]
+    def LOAD():
+        return [ExecutionUnit.LSU0, ExecutionUnit.LU0, ExecutionUnit.LU1]
+    def STORE():
+        return [ExecutionUnit.LSU0, ExecutionUnit.SU0]
+
+# Opaque functions called by SLOTHY to add further microarchitecture-
+# specific constraints which are not encapsulated by the general framework.
+def add_further_constraints(slothy):
+    if slothy.config.constraints.functional_only:
+        return
+    slothy.restrict_slots_for_instructions_by_property(
+        is_neon_instruction, [0,1,2,3])
+    slothy.restrict_slots_for_instructions_by_class(
+        [aesr_x4, aesr_x4], [0]
+    )
+
+def has_min_max_objective(config):
+    return False
+def get_min_max_objective(slothy):
+    return
+
+### TODO: Copy-pasted from A72 model and roughly adjusted
+
+execution_units = {
+    (vmul, vmul_lane,
+     vmla, vmla_lane,
+     vmls, vmls_lane,
+     vqrdmulh, vqrdmulh_lane,
+     vqdmulh_lane)
+    : ExecutionUnit.V(),
+
+    (vadd, vsub,
+     trn1, trn2 )
+    : ExecutionUnit.V(),
+
+    Vins : ExecutionUnit.V(),
+    umov_d : ExecutionUnit.V(), # ???
+
+    ( Ldr_Q, Ldr_X )
+    : ExecutionUnit.LOAD(),
+
+    ( Str_Q, Str_X )
+    : ExecutionUnit.STORE(),
+
+    (add, add_imm) : ExecutionUnit.I(),
+    (add_lsl, add_lsr) : list(map(list, combinations(ExecutionUnit.I(), 2))),
+
+    vsrshr : ExecutionUnit.V(),
+
+    St4 : list(map(list, product(ExecutionUnit.STORE(), ExecutionUnit.V()))),
+
+    Ld4 : list(map(list, (product(map(list, combinations(ExecutionUnit.LOAD(), 2)), ExecutionUnit.V()))))
+}
+
+inverse_throughput = {
+    (vmul, vmul_lane,
+     vqrdmulh, vqrdmulh_lane,
+     vmla, vmla_lane,
+     vmls, vmls_lane,
+     vqdmulh_lane)
+    : 1,  # 4/(1/4) = 16
+
+    (vadd, vsub,
+     trn1, trn2)
+    : 1,
+
+    Vins : 1,
+    umov_d : 2,
+
+    (add, add_imm) : 1,
+    (add_lsl, add_lsr) : 1,
+
+    ( Ldr_Q,
+      Str_Q,
+      stack_vld1r, stack_vld2_lane, Ldr_X, Str_X )
+      : 1,
+
+    vsrshr : 1,
+
+    St4 : 6,  # or maybe 12?
+    Ld4 : 6  # or maybe 12?
+}
+
+### REVISIT
+default_latencies = {
+    (vmul, vmul_lane,
+     vqrdmulh, vqrdmulh_lane,
+     vmls, vmls_lane,
+     vmla, vmla_lane,
+     vqdmulh_lane)
+    : 3,
+
+    (vadd, vsub,
+     trn1, trn2 )
+    : 2, # Approximation -- not necessary to get it exactly right, as mentioned above
+
+    ( Ldr_Q, Ldr_X,
+      Str_Q, Str_X,
+      stack_vld1r, stack_vld2_lane )
+      : 4, # approx
+
+    Vins : 2, # approx
+    umov_d : 10, # approx, worst case
+
+    (add, add_imm) : 1,
+    (add_lsl, add_lsr) : 2,
+
+    vsrshr : 3, # approx
+    St4 : 8,  # guessed
+    Ld4 : 4  # guessed
+}
+
+def get_latency(src, out_idx, dst):
+    instclass_src = find_class(src)
+    instclass_dst = find_class(dst)
+
+    latency = lookup_multidict(default_latencies, src)
+    return latency
+
+def get_units(src):
+    units = lookup_multidict(execution_units, src)
+    if isinstance(units,list):
+        return units
+    else:
+        return [units]
+
+def get_inverse_throughput(src):
+    return lookup_multidict(inverse_throughput, src)
