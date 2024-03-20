7b9b9ab65062560750fb3f629d15a0a94ec5e778
Hanno Becker <hannobecker@posteo.de>
2024-03-20 05:20:32 +0000

    Don't assume that CI has llvm-mca

--- tutorial/tutorial_all.sh
+++ tutorial/tutorial_all.sh
@@ -10,8 +10,13 @@ python3 tutorial-3b.py >/dev/null
 echo "* tutorial-4.py (Software pipelining)"
 python3 tutorial-4.py >/dev/null
 
-echo "* tutorial-5.py (Running SLOTHY with LLVM-MCA)"
-python3 tutorial-5.py >/dev/null
+if [ -x "$(command -v llvm-mca)" ]
+then
+    echo "* tutorial-5.py (Running SLOTHY with LLVM-MCA)"
+    python3 tutorial-5.py >/dev/null
+else
+    echo "* tutorial-5.py (Running SLOTHY with LLVM-MCA) SKIP"
+fi
 
 echo "* tutorial-6.py (Optimizing a full Neon NTT)"
 python3 tutorial-6.py >/dev/null
