5c656b9303cb0b636da4bfb940e1bc868f679b50
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-10-19 14:23:05 +0800

    tweak dilithium intt to not use explicit pointer increments

--- examples/naive/armv7m/intt_dilithium_123_456_78.s
+++ examples/naive/armv7m/intt_dilithium_123_456_78.s
@@ -248,21 +248,21 @@ pqcrystals_dilithium_invntt_tomont:
     ldr.w pol3, [ptr_p, #7*distance2/4]
     _3_layer_inv_butterfly_light_fast_first pol0, pol1, pol2, pol3, pol4, pol5, pol6, pol7, s2, s3, s4, s5, s6, s7, s8, zeta, qinv, q, temp_h, temp_l
     
-    ldr.w pol0, [ptr_p]
-    ldr.w pol1, [ptr_p, #1*distance2/4]
-    ldr.w pol2, [ptr_p, #2*distance2/4]
-    ldr.w pol3, [ptr_p, #3*distance2/4]
+    ldr.w pol0, [ptr_p], #128
+    ldr.w pol1, [ptr_p, #1*distance2/4-128]
+    ldr.w pol2, [ptr_p, #2*distance2/4-128]
+    ldr.w pol3, [ptr_p, #3*distance2/4-128]
     _3_layer_inv_butterfly_light_fast_second pol0, pol1, pol2, pol3, pol4, pol5, pol6, pol7, s2, s3, s4, s5, s6, s7, s8, zeta, qinv, q, temp_h, temp_l
 
-    str.w pol1, [ptr_p, #1*distance2/4]
-    str.w pol2, [ptr_p, #2*distance2/4]
-    str.w pol3, [ptr_p, #3*distance2/4]
-    str.w pol4, [ptr_p, #4*distance2/4]
-    str.w pol5, [ptr_p, #5*distance2/4]
-    str.w pol6, [ptr_p, #6*distance2/4]
-    str.w pol7, [ptr_p, #7*distance2/4]
-    str.w pol0, [ptr_p]
-    add.w ptr_p, #strincr2
+    str.w pol1, [ptr_p, #1*distance2/4-128]
+    str.w pol2, [ptr_p, #2*distance2/4-128]
+    str.w pol3, [ptr_p, #3*distance2/4-128]
+    str.w pol5, [ptr_p, #5*distance2/4-128]
+    str.w pol6, [ptr_p, #6*distance2/4-128]
+    str.w pol7, [ptr_p, #7*distance2/4-128]
+    str.w pol0, [ptr_p, #-128]
+    str.w pol4, [ptr_p], #128
+    //add.w ptr_p, #strincr2
 
     vmov temp_l, s10
     cmp.w temp_l, ptr_p
@@ -281,26 +281,26 @@ pqcrystals_dilithium_invntt_tomont:
     vldm ptr_zeta!, {s2-s8}
     vmov s0, ptr_zeta
     layer456_loop:
-	    ldr.w pol0, [ptr_p]
-	    ldr.w pol1, [ptr_p, #1*distance2/4]
-	    ldr.w pol2, [ptr_p, #2*distance2/4]
-	    ldr.w pol3, [ptr_p, #3*distance2/4]
-	    ldr.w pol4, [ptr_p, #4*distance2/4]
-	    ldr.w pol5, [ptr_p, #5*distance2/4]
-	    ldr.w pol6, [ptr_p, #6*distance2/4]
-	    ldr.w pol7, [ptr_p, #7*distance2/4]
+	    ldr.w pol0, [ptr_p], #128
+	    ldr.w pol1, [ptr_p, #1*distance2/4-128]
+	    ldr.w pol2, [ptr_p, #2*distance2/4-128]
+	    ldr.w pol3, [ptr_p, #3*distance2/4-128]
+	    ldr.w pol4, [ptr_p, #4*distance2/4-128]
+	    ldr.w pol5, [ptr_p, #5*distance2/4-128]
+	    ldr.w pol6, [ptr_p, #6*distance2/4-128]
+	    ldr.w pol7, [ptr_p, #7*distance2/4-128]
 
 	    _3_layer_inv_CT_32 pol0, pol1, pol2, pol3, pol4, pol5, pol6, pol7, s2, s3, s4, s5, s6, s7, s8, zeta, qinv, q, temp_h, temp_l
 
-	    str.w pol1, [ptr_p, #1*distance2/4]
-	    str.w pol2, [ptr_p, #2*distance2/4]
-	    str.w pol3, [ptr_p, #3*distance2/4]
-	    str.w pol4, [ptr_p, #4*distance2/4]
-	    str.w pol5, [ptr_p, #5*distance2/4]
-	    str.w pol6, [ptr_p, #6*distance2/4]
-	    str.w pol7, [ptr_p, #7*distance2/4]
-	    str.w pol0, [ptr_p]
-	    add.w ptr_p, #strincr2
+	    str.w pol1, [ptr_p, #1*distance2/4-128]
+	    str.w pol2, [ptr_p, #2*distance2/4-128]
+	    str.w pol3, [ptr_p, #3*distance2/4-128]
+	    str.w pol5, [ptr_p, #5*distance2/4-128]
+	    str.w pol6, [ptr_p, #6*distance2/4-128]
+	    str.w pol7, [ptr_p, #7*distance2/4-128]
+	    str.w pol0, [ptr_p, #-128]
+      str.w pol4, [ptr_p], #128
+	    //add.w ptr_p, #strincr2
 
       vmov temp_l, s10
       cmp.w ptr_p, temp_l
