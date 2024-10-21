d86c27ec4a76fda2b69f002b05aa225578f5bc1c
Hanno Becker <beckphan@amazon.co.uk>
2024-10-21 04:08:01 +0100

    Improve debuggabiliity by adding line numbers to code dump

--- slothy/helper.py
+++ slothy/helper.py
@@ -258,8 +258,8 @@ class SourceLine:
         if len(s) == 0:
             return
         fun(f"Dump: {name}")
-        for l in s:
-            fun(f"> {l.to_string()}")
+        for (i,l) in enumerate(s):
+            fun(f"[{i}]> {l.to_string()}")
 
     def set_text(self, s):
         """Set the text of the source line
