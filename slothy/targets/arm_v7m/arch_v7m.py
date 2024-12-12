8b12ca2476d9fda1f0948539422738b8da55d5d1
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-12-12 18:37:18 +0800

    fix number of FPU registers

--- slothy/targets/arm_v7m/arch_v7m.py
+++ slothy/targets/arm_v7m/arch_v7m.py
@@ -115,7 +115,7 @@ class RegisterType(Enum):
         """Return the list of all registers of a given type"""
 
         gprs_normal  = [ f"r{i}" for i in range(15) ]
-        fprs_normal  = [ f"s{i}" for i in range(31) ]
+        fprs_normal  = [ f"s{i}" for i in range(32) ]
 
         gprs_extra  = []
         fprs_extra  = []
