549bc31f1386da213093e060e616b9e4df195671
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-12-15 10:58:19 +0800

    add missing slothy annotations re dependencies through memory

--- examples/naive/armv7m/armv7m_simple0.s
+++ examples/naive/armv7m/armv7m_simple0.s
@@ -1,32 +1,32 @@
 
 start:
-ldr r1, [r0,   #4]
+ldr r1, [r0,   #4] // @slothy:reads=a
 add r1, r2,r1
 eor.w r1,r1, r3
 smlabt r3,r2, r2, r1
 asrs r3,   r3,#1
-str r3, [r0,#4]
+str r3, [r0,#4] // @slothy:writes=a
 
-ldm r0, {r1-r2,r14}
+ldm r0, {r1-r2,r14} // @slothy:reads=a
 add r1, r2,r1
 eor.w r1,r1, r14
 smlabt r3,r2, r2, r1
 asrs r3,   r3,#1
-str r3, [r0,#4]
+str r3, [r0,#4] // @slothy:writes=a
 
 
-ldm r0, {r1-r3}
+ldm r0, {r1-r3} // @slothy:reads=a
 add r1, r2,r1
 eor.w r1,r1, r3
 smlabt r3,r2, r2, r1
 asrs r3,   r3,#1
-str r3, [r0,#4]
+str r3, [r0,#4] // @slothy:writes=a
 
-ldm r0, {r1,r2,r3}
+ldm r0, {r1,r2,r3} // @slothy:reads=a
 add r1, r2,r1
 eor.w r1,r1, r3
 smlabt r3,r2, r2, r1
 asrs r3,   r3,#1
-str r3, [r0,#4]
+str r3, [r0,#4] // @slothy:writes=a
 
 end:
