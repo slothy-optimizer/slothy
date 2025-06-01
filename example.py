68c607db8b50a0c3e1e95b0a0176f36b4ba9a814
Justus Bergermann <justus.bergermann@mpi-sp.org>
2025-06-01 17:48:44 +0200

    refactor: satisfy linter

--- example.py
+++ example.py
@@ -1767,7 +1767,20 @@ class RISC_VExample0(Example):
         slothy.config.variable_size = True
         slothy.config.constraints.stalls_first_attempt = 32
         slothy.config.inputs_are_outputs = True
-        slothy.config.outputs = ['v0', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9', 'v10', 'v11']
+        slothy.config.outputs = [
+            "v0",
+            "v1",
+            "v2",
+            "v3",
+            "v4",
+            "v5",
+            "v6",
+            "v7",
+            "v8",
+            "v9",
+            "v10",
+            "v11",
+        ]
         slothy.optimize()
 
 
@@ -1868,6 +1881,7 @@ class RISC_V_poly_basemul_8l_acc_rv64im(Example):
         slothy.config.reserved_regs = r
         slothy.optimize_loop("poly_basemul_8l_acc_rv64im_looper")
 
+
 class RISC_V_ntt_rvv(Example):
     def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
         name = "ntt_rvv"
@@ -1897,9 +1911,10 @@ class RISC_V_ntt_rvv(Example):
         r = slothy.config.reserved_regs
         r += ["x3"]
         slothy.config.reserved_regs = r
-        slothy.config.outputs = ['v17']
+        slothy.config.outputs = ["v17"]
         slothy.config.allow_useless_instructions = True
         slothy.optimize("start", "end")
+
 
 class RISC_V_test(Example):
     def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
@@ -1912,21 +1927,59 @@ class RISC_V_test(Example):
             infile += f"_{var}"
         name += f"_{target_label_dict[target]}"
 
-        super().__init__(infile, name, rename=True, arch=arch, target=target, funcname="test", timeout=timeout)
+        super().__init__(
+            infile,
+            name,
+            rename=True,
+            arch=arch,
+            target=target,
+            funcname="test",
+            timeout=timeout,
+        )
 
-    def core(self,slothy):
-        slothy.config.variable_size=True
-        slothy.config.constraints.stalls_first_attempt=32
+    def core(self, slothy):
+        slothy.config.variable_size = True
+        slothy.config.constraints.stalls_first_attempt = 32
         slothy.config.inputs_are_outputs = True
 
         r = slothy.config.reserved_regs
-        r += ['x3']
+        r += ["x3"]
         slothy.config.reserved_regs = r
-        slothy.config.outputs = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10',
-                                 'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17', 'x18', 'x19',
-                                 'x20', 'x21', 'x22', 'x23', 'x24', 'x25', 'x26', 'x27', 'x28',
-                                 'x29', 'x30', 'x31']
+        slothy.config.outputs = [
+            "x1",
+            "x2",
+            "x3",
+            "x4",
+            "x5",
+            "x6",
+            "x7",
+            "x8",
+            "x9",
+            "x10",
+            "x11",
+            "x12",
+            "x13",
+            "x14",
+            "x15",
+            "x16",
+            "x17",
+            "x18",
+            "x19",
+            "x20",
+            "x21",
+            "x22",
+            "x23",
+            "x24",
+            "x25",
+            "x26",
+            "x27",
+            "x28",
+            "x29",
+            "x30",
+            "x31",
+        ]
         slothy.optimize(start="start_label", end="end_label")
+
 
 ########################################################################################
 
