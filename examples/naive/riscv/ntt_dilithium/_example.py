ca841c0fe45f2f5183405a1b749081eb1477fb97
Amin Abdulrahman <amin@abdulrahman.de>
2025-06-24 05:53:35 -0400

    feat: Add more ntt-related rvv dilithium examples

--- examples/naive/riscv/ntt_dilithium/_example.py
+++ examples/naive/riscv/ntt_dilithium/_example.py
@@ -220,10 +220,129 @@ class RISC_V_ntt_rvv_vlen128(OptimizationRunner):
         slothy.optimize("start", "end")
 
 
+class RISC_V_normal2ntt_order_rvv_vlen128(OptimizationRunner):
+    def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
+        name = "dilithium_normal2ntt_order_rvv_vlen128"
+        infile = name
+
+        if var != "":
+            name += f"_{var}"
+            infile += f"_{var}"
+        # name += f"_{target_label_dict[target]}"
+
+        super().__init__(
+            infile,
+            name,
+            subfolder=SUBFOLDER,
+            rename=True,
+            arch=arch,
+            target=target,
+            funcname="normal2ntt_order_rvv_vlen128",
+            timeout=timeout,
+        )
+
+    def core(self, slothy):
+        slothy.config.variable_size = True
+        slothy.config.constraints.stalls_first_attempt = 32
+        slothy.config.inputs_are_outputs = True
+
+        slothy.config.sw_pipelining.enabled = True
+        slothy.config.sw_pipelining.halving_heuristic = True
+        slothy.config.split_heuristic = True
+        slothy.config.split_heuristic_factor = 5
+        slothy.config.split_heuristic_repeat = 2
+        slothy.config.split_heuristic_stepsize = 0.05
+
+        r = slothy.config.reserved_regs
+        r += ["x3"]
+        slothy.config.reserved_regs = r
+        slothy.optimize_loop("normal2ntt_order_rvv_vlen128_loop")
+
+
+class RISC_V_ntt2normal_order_rvv_vlen128(OptimizationRunner):
+    def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
+        name = "dilithium_ntt2normal_order_rvv_vlen128"
+        infile = name
+
+        if var != "":
+            name += f"_{var}"
+            infile += f"_{var}"
+        # name += f"_{target_label_dict[target]}"
+
+        super().__init__(
+            infile,
+            name,
+            subfolder=SUBFOLDER,
+            rename=True,
+            arch=arch,
+            target=target,
+            funcname="ntt2normal_order_rvv_vlen128",
+            timeout=timeout,
+        )
+
+    def core(self, slothy):
+        slothy.config.variable_size = True
+        slothy.config.constraints.stalls_first_attempt = 32
+        slothy.config.inputs_are_outputs = True
+
+        slothy.config.sw_pipelining.enabled = True
+        slothy.config.sw_pipelining.halving_heuristic = True
+        slothy.config.split_heuristic = True
+        slothy.config.split_heuristic_factor = 5
+        slothy.config.split_heuristic_repeat = 2
+        slothy.config.split_heuristic_stepsize = 0.05
+
+        r = slothy.config.reserved_regs
+        r += ["x3"]
+        slothy.config.reserved_regs = r
+        slothy.optimize_loop("ntt2normal_order_rvv_vlen128_loop")
+
+
+class RISC_V_poly_reduce_rvv_vlen128(OptimizationRunner):
+    def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
+        name = "dilithium_poly_reduce_rvv_vlen128"
+        infile = name
+
+        if var != "":
+            name += f"_{var}"
+            infile += f"_{var}"
+
+        super().__init__(
+            infile,
+            name,
+            subfolder=SUBFOLDER,
+            rename=True,
+            arch=arch,
+            target=target,
+            funcname="poly_reduce_rvv_vlen128",
+            timeout=timeout,
+        )
+
+    def core(self, slothy):
+        slothy.config.variable_size = True
+        slothy.config.constraints.stalls_first_attempt = 32
+        slothy.config.inputs_are_outputs = True
+
+        slothy.config.sw_pipelining.enabled = True
+        slothy.config.sw_pipelining.halving_heuristic = True
+        slothy.config.split_heuristic = True
+        slothy.config.split_heuristic_factor = 5
+        slothy.config.split_heuristic_repeat = 2
+        slothy.config.split_heuristic_stepsize = 0.05
+
+        r = slothy.config.reserved_regs
+        r += ["x3"]
+        slothy.config.reserved_regs = r
+        slothy.optimize_loop("poly_reduce_rvv_vlen128_loop")
+
+
 example_instances = [
     RISC_V_ntt8l_singleissue_plant_rv64im(target=Target_XuanTieC908, timeout=300),
     RISC_V_ntt8l_dualissue_plant_rv64im(timeout=300),
     RISC_V_intt8l_dualissue_plant_rv64im(),
     RISC_V_intt8l_singleissue_plant_rv64im(),
     RISC_V_ntt_rvv_vlen128(target=Target_XuanTieC908),
+    RISC_V_normal2ntt_order_rvv_vlen128(),
+    RISC_V_ntt2normal_order_rvv_vlen128(),
+    RISC_V_poly_reduce_rvv_vlen128(),
 ]
