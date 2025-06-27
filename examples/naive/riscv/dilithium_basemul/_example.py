3b2fbd0256609e3e3aa0cd5a90d0f0ccc78029c0
Amin Abdulrahman <amin@abdulrahman.de>
2025-06-27 07:13:10 -0400

    feat: Add poly_reduce dilithium for RV64

--- examples/naive/riscv/dilithium_basemul/_example.py
+++ examples/naive/riscv/dilithium_basemul/_example.py
@@ -136,6 +136,39 @@ class RISC_V_poly_basemul_8l_acc_end_rv64im(OptimizationRunner):
         slothy.optimize_loop("poly_basemul_8l_acc_end_rv64im_looper")
 
 
+class RISC_V_poly_reduce_rv64im(OptimizationRunner):
+    def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
+        name = "dilithium_poly_reduce_rv64im"
+        infile = name
+
+        if var != "":
+            name += f"_{var}"
+            infile += f"_{var}"
+
+        super().__init__(
+            infile,
+            name,
+            subfolder=SUBFOLDER,
+            rename=True,
+            arch=arch,
+            target=target,
+            timeout=timeout,
+            funcname="poly_reduce_rv64im",
+        )
+
+    def core(self, slothy):
+        slothy.config.variable_size = True
+        slothy.config.constraints.stalls_first_attempt = 32
+        slothy.config.inputs_are_outputs = True
+        slothy.config.sw_pipelining.enabled = True
+
+        r = slothy.config.reserved_regs
+        r += ["x3"]
+        slothy.config.reserved_regs = r
+        slothy.config.outputs = ["x3"]
+        slothy.optimize_loop("poly_reduce_rv64im_loop")
+
+
 class RISC_V_poly_basemul_rvv_vlen128(OptimizationRunner):
     def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
         name = "dilithium_poly_basemul_rvv_vlen128"
