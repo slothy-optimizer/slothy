1d5c7aff0ff9c94ecaf98186f77403acddfd4134
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-07-05 21:11:21 +0800

    A72: Add vext to uArch model
    Add vext instruction class to Cortex-A72 target model to support
    vector extract operations with correct performance characteristics
    from A72 SWOG Section 3.14:
    
    - Execution latency: 3 cycles
    - Execution throughput: 1 instruction/cycle
    - Pipeline: ASIMD0 or ASIMD1
    
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- tests/naive/aarch64/instructions.s
+++ tests/naive/aarch64/instructions.s
@@ -2,6 +2,7 @@ start:
 
 // TODO: this is currently incomplete. We should add all instructions
 
+ext v0.16b, v1.16b, v2.16b, #8
 aese v0.16b, v1.16b
 aesmc v2.16b, v3.16b
 pmull v4.1q, v5.1d, v6.1d
