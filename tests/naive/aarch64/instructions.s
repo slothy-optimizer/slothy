a4324a89d130c2ec2b0768792e71fe384e859a2a
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-07-05 21:32:47 +0800

    AArch64: Extend instructions test with and and eor
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- tests/naive/aarch64/instructions.s
+++ tests/naive/aarch64/instructions.s
@@ -2,6 +2,8 @@ start:
 
 // TODO: this is currently incomplete. We should add all instructions
 
+and v22.16b, v23.16b, v24.16b
+eor v28.16b, v29.16b, v30.16b
 ext v0.16b, v1.16b, v2.16b, #8
 aese v0.16b, v1.16b
 aesmc v2.16b, v3.16b
