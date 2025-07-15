f1b1a551961da83de62f1b2ac9d4e835484e1abe
Amin Abdulrahman <amin@abdulrahman.de>
2025-07-15 11:56:03 -0400

    riscv: add spill of regs to dilithium rvv basemul

--- examples/naive/riscv/dilithium_basemul/dilithium_poly_basemul_rvv_vlen128.s
+++ examples/naive/riscv/dilithium_basemul/dilithium_poly_basemul_rvv_vlen128.s
@@ -838,9 +838,48 @@
 # inv256qinv <- low(inv256*qinv)
 .equ inv256qinv, 4286571514
 
+.macro save_regs
+  sd s0,  0*8(sp)
+  sd s1,  1*8(sp)
+  sd s2,  2*8(sp)
+  sd s3,  3*8(sp)
+  sd s4,  4*8(sp)
+  sd s5,  5*8(sp)
+  sd s6,  6*8(sp)
+  sd s7,  7*8(sp)
+  sd s8,  8*8(sp)
+  sd s9,  9*8(sp)
+  sd s10, 10*8(sp)
+  sd s11, 11*8(sp)
+  sd gp,  12*8(sp)
+  sd tp,  13*8(sp)
+  sd ra,  14*8(sp)
+.endm
+
+.macro restore_regs
+  ld s0,  0*8(sp)
+  ld s1,  1*8(sp)
+  ld s2,  2*8(sp)
+  ld s3,  3*8(sp)
+  ld s4,  4*8(sp)
+  ld s5,  5*8(sp)
+  ld s6,  6*8(sp)
+  ld s7,  7*8(sp)
+  ld s8,  8*8(sp)
+  ld s9,  9*8(sp)
+  ld s10, 10*8(sp)
+  ld s11, 11*8(sp)
+  ld gp,  12*8(sp)
+  ld tp,  13*8(sp)
+  ld ra,  14*8(sp)
+.endm
+
 .globl poly_basemul_rvv_vlen128
 .align 2
 poly_basemul_rvv_vlen128:
+    addi sp, sp, -8*15    # Allocate stack space
+    save_regs             # Save all callee-saved registers
+    
     li a7, 4*8
     li t2, 8
     vsetvli a7, a7, e32, m8, tu, mu
@@ -856,4 +895,7 @@ poly_basemul_rvv_vlen128_loop:
     addi a0, a0, 4*4*8
     addi t2, t2, -1
     bnez t2, poly_basemul_rvv_vlen128_loop
+    
+    restore_regs          # Restore all saved registers
+    addi sp, sp, 8*15     # Deallocate stack space
 ret
