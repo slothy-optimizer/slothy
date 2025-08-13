dec3f5dc3a02ccd21721f82572531a467aaa15d0
Justus Bergermann <justus.bergermann@mpi-sp.org>
2025-08-13 12:06:11 +0200

    feat: add first hybrid keccak example config

--- examples/naive/riscv/keccak/_example.py
+++ examples/naive/riscv/keccak/_example.py
@@ -42,6 +42,42 @@ class RISC_V_fips202_rv32im(OptimizationRunner):
         slothy.optimize_loop("loop_start")
 
 
+class RISC_V_fips202_rv32imbv_hybrid_x3(OptimizationRunner):
+    def __init__(self, var="", arch=RISC_V, target=Target_XuanTieC908, timeout=None):
+        name = "fips202_rv32imbv_hybrid_x3"
+        infile = name
+
+        super().__init__(
+            infile,
+            name,
+            subfolder=SUBFOLDER,
+            rename=True,
+            arch=arch,
+            target=target,
+            timeout=timeout,
+            funcname="KeccakF1600_StatePermute_RV32V_3x",
+            var=var,
+        )
+
+    def core(self, slothy):
+        slothy.config.variable_size = True
+        slothy.config.constraints.stalls_first_attempt = 32
+        slothy.config.inputs_are_outputs = True
+        slothy.config.sw_pipelining.enabled = True
+        slothy.config.sw_pipelining.halving_heuristic = True
+        slothy.config.split_heuristic = True
+        slothy.config.split_heuristic_factor = 7
+        slothy.config.split_heuristic_repeat = 1
+        slothy.config.split_heuristic_stepsize = 0.1
+
+        r = slothy.config.reserved_regs
+        r += ["x3"]
+        slothy.config.reserved_regs = r
+        # slothy.optimize("start", "end")
+        slothy.optimize_loop("loop_start")
+
+
 example_instances = [
     RISC_V_fips202_rv32im(),
+    RISC_V_fips202_rv32imbv_hybrid_x3(),
 ]
