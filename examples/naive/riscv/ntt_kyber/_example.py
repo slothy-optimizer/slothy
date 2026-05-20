12aeb1343f4577a66bb6c0e1fe1dea58d22af3df
Justus Bergermann <justus.bergermann@mpi-sp.org>
2026-05-20 11:31:10 +0200

    optimize ntt_kyber_rvv_vlen128 with better params

--- examples/naive/riscv/ntt_kyber/_example.py
+++ examples/naive/riscv/ntt_kyber/_example.py
@@ -227,9 +227,9 @@ class RISC_V_ntt_rvv_vlen128(OptimizationRunner):
         # slothy.config.sw_pipelining.enabled = True
         # slothy.config.sw_pipelining.halving_heuristic = True
         slothy.config.split_heuristic = True
-        slothy.config.split_heuristic_factor = 20
-        slothy.config.split_heuristic_repeat = 1
-        slothy.config.split_heuristic_stepsize = 0.2
+        slothy.config.split_heuristic_factor = 10
+        slothy.config.split_heuristic_repeat = 2
+        slothy.config.split_heuristic_stepsize = 0.1
         slothy.config.split_heuristic_estimate_performance = False
 
         r = slothy.config.reserved_regs
