be1d4f56568a9575083e3a38bbad31548b7fe40d
justus.bergerman <justus.bergermann@octopus.celab.mpi-sp.org>
2026-06-22 19:51:17 +0000

    optimized remaining kyber examples

--- examples/naive/riscv/ntt_kyber/_example.py
+++ examples/naive/riscv/ntt_kyber/_example.py
@@ -250,10 +250,10 @@ class RISC_V_intt_rvv_vlen128(OptimizationRunner):
 
         slothy.config.allow_useless_instructions = True
         slothy.config.split_heuristic = True
-        slothy.config.split_heuristic_factor = 5
+        slothy.config.split_heuristic_factor = 23
         slothy.config.split_heuristic_repeat = 2
         slothy.config.split_heuristic_stepsize = 0.05
-
+        slothy.config.timeout = 180
 
         slothy.config.outputs = ["x17"]  # TODO: this does not do anything
         r = slothy.config.reserved_regs
