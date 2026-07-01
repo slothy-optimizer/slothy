6d37415387b6ee21bce85f11f51a68cfb173b08b
justus.bergerman <justus.bergermann@octopus.celab.mpi-sp.org>
2026-07-01 13:31:01 +0000

    opt cbd2

--- examples/naive/riscv/kyber_sampling/_example.py
+++ examples/naive/riscv/kyber_sampling/_example.py
@@ -37,8 +37,8 @@ class RISC_V_cbd2_rvv_vlen128(OptimizationRunner):
         slothy.config.constraints.stalls_first_attempt = 32
         slothy.config.inputs_are_outputs = True
 
-        slothy.config.sw_pipelining.enabled = True
-        slothy.config.sw_pipelining.halving_heuristic = True
+        #slothy.config.sw_pipelining.enabled = True
+        #slothy.config.sw_pipelining.halving_heuristic = True
         slothy.config.split_heuristic = True
         slothy.config.split_heuristic_factor = 5
         slothy.config.split_heuristic_repeat = 2
